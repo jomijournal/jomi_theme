d84c7f9855345584ebb052bc9a77b23c5a3cb5ba
Philip Seyfi <seifip@gmail.com>
2014-07-02 01:46:05 -0400

    updated single post styles

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0c2367179dd2023ea488089733f62982');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c51c36247298002e2c6e86ab8100a5d1');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline

c0a519d6686042b360c86d9b093754de6bc364ae
Albert Chen <albert@jomi.com>
2014-07-03 11:53:11 -0400

    prettified and fixed footer for mobile

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '633cef91c774014097d2f140f1f9af68');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2d4a5e929c46e8d36a58642b53a46ae8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/front-footer.php
+++ templates/front-footer.php
@@ -1,10 +1,23 @@
-<footer class="row">
-  <h4>Preview content on:</h4>
-  <nav class="nav-bottom">
-    <ul>
-      <li><a href="/category/paediatry/" class="border">Paediatry</a></li>
-      <li><a href="/category/neurology/" class="border">Neurology</a></li>
-      <li><a href="/category/otolaryngology/" class="border">Otolaryngology</a></li>
-    </ul>
-  </nav>
+<footer>
+  <div class='row'>
+	  <h4>Preview content on:</h4>
+  </div>
+  <div class='row'>
+	  <nav class="nav-bottom">
+	    <ul>
+	      <div class='col-sm-2 col-sm-offset-2'>
+	      	<li><a href="/category/paediatry/" class="border">Paediatry</a></li>
+	      </div>
+	      <div class='col-sm-2'>
+	      	<li><a href="/category/neurology/" class="border">Neurology</a></li>
+	      </div>
+	      <div class='col-sm-2'>
+	      	<li><a href="/category/otolaryngology/" class="border">Otolaryngology</a></li>
+	      </div>
+	      <div class='col-sm-2'>
+	      	<li><a href="/category/poop/" class="border">Poop</a></li>
+	      </div>
+	    </ul>
+	  </nav>
+  </div>
 </footer>

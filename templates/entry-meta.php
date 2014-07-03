b462666c3dc8952c45786314146217f09412f4c2
Philip Seyfi <seifip@gmail.com>
2014-07-03 17:02:13 -0400

    multiple authors/guest authors

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,2 +1,10 @@
-<time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
-<p class="byline author vcard"><?php echo __('By', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></p>
+<time class="published" datetime="<?php echo get_the_time('c'); ?>">Published on <?php echo get_the_date(); ?></time>
+<p class="byline author vcard">
+<?php
+if ( function_exists( 'coauthors_posts_links' ) ) {
+    coauthors_posts_links();
+} else {
+    the_author_posts_link();
+}
+?>
+</a></p>

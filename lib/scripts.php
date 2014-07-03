439d31986f22035ab605f7a7ce0701e1a626a361
Philip Seyfi <seifip@gmail.com>
2014-07-03 14:25:22 -0400

    fixed header spacing

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7859ed374917e7508b4e9899dbb24ce2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e0522726f881dfea514ea625dad319d5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline

80f665503576299a23067d79b58779078a2e0074
Albert Chen <albert@jomi.com>
2014-07-03 15:10:34 -0400

    front-footer set to fixed (temporary)

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6baea5793547a873c376002f823bac9b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '090902c6e7ad646adbf42ce4f68c6ca2');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline

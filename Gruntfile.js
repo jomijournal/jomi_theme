cf868ecb1ac69beeae36420b5d2603a6e3a75eed
Albert Chen <albert@jomi.com>
2014-07-08 19:31:19 -0400

    new and improved gruntfile. now with build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,26 @@
 'use strict';
 module.exports = function(grunt) {
+  // Load all tasks
+  require('load-grunt-tasks')(grunt);
+  // Show elapsed time
+  require('time-grunt')(grunt);
+
+  var jsFileList = [
+    'assets/vendor/bootstrap/js/transition.js',
+    'assets/vendor/bootstrap/js/alert.js',
+    'assets/vendor/bootstrap/js/button.js',
+    'assets/vendor/bootstrap/js/carousel.js',
+    'assets/vendor/bootstrap/js/collapse.js',
+    'assets/vendor/bootstrap/js/dropdown.js',
+    'assets/vendor/bootstrap/js/modal.js',
+    'assets/vendor/bootstrap/js/tooltip.js',
+    'assets/vendor/bootstrap/js/popover.js',
+    'assets/vendor/bootstrap/js/scrollspy.js',
+    'assets/vendor/bootstrap/js/tab.js',
+    'assets/vendor/bootstrap/js/affix.js',
+    'assets/js/plugins/*.js',
+    'assets/js/_*.js'
+  ];
 
   grunt.initConfig({
     jshint: {
@@ -9,11 +30,12 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'assets/js/*.js',
-        '!assets/js/scripts.min.js'
+        '!assets/js/scripts.js',
+        '!assets/**/*.min.*'
       ]
     },
     less: {
-      dist: {
+      dev: {
         files: {
           'assets/css/main.min.css': [
             'assets/less/app.less'
@@ -23,37 +45,64 @@ module.exports = function(grunt) {
           compress: true,
           // LESS source map
           // To enable, set sourceMap to true and update sourceMapRootpath based on your install
-          sourceMap: false,
+          sourceMap: true,
           sourceMapFilename: 'assets/css/main.min.css.map',
           sourceMapRootpath: '/app/themes/roots/'
         }
+      },
+      build: {
+        files: {
+          'assets/css/main.min.css': [
+            'assets/less/app.less'
+          ]
+        },
+        options: {
+          compress: true
+        }
       }
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: [jsFileList],
+        dest: 'assets/js/scripts.js',
+      },
     },
     uglify: {
       dist: {
         files: {
-          'assets/js/scripts.min.js': [
-            'assets/js/plugins/bootstrap/transition.js',
-            'assets/js/plugins/bootstrap/alert.js',
-            'assets/js/plugins/bootstrap/button.js',
-            'assets/js/plugins/bootstrap/carousel.js',
-            'assets/js/plugins/bootstrap/collapse.js',
-            'assets/js/plugins/bootstrap/dropdown.js',
-            'assets/js/plugins/bootstrap/modal.js',
-            'assets/js/plugins/bootstrap/tooltip.js',
-            'assets/js/plugins/bootstrap/popover.js',
-            'assets/js/plugins/bootstrap/scrollspy.js',
-            'assets/js/plugins/bootstrap/tab.js',
-            'assets/js/plugins/bootstrap/affix.js',
-            'assets/js/plugins/*.js',
-            'assets/js/_*.js'
+          'assets/js/scripts.min.js': [jsFileList]
+        }
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12']
+      },
+      dev: {
+        options: {
+          map: 'assets/css/'
+        },
+        src: 'assets/css/main.css'
+      },
+      build: {
+        src: 'assets/css/main.min.css'
+      }
+    },
+    modernizr: {
+      build: {
+        devFile: 'assets/vendor/modernizr/modernizr.js',
+        outputFile: 'assets/js/vendor/modernizr.min.js',
+        files: {
+          'src': [
+            ['assets/js/scripts.min.js'],
+            ['assets/css/main.min.css']
           ]
         },
-        options: {
-          // JS source map: to enable, uncomment the lines below and update sourceMappingURL based on your install
-          // sourceMap: 'assets/js/scripts.min.js.map',
-          // sourceMappingURL: '/app/themes/roots/assets/js/scripts.min.js.map'
-        }
+        uglify: true,
+        parseFiles: true
       }
     },
     version: {
@@ -69,55 +118,49 @@ module.exports = function(grunt) {
       less: {
         files: [
           'assets/less/*.less',
-          'assets/less/bootstrap/*.less'
+          'assets/less/**/*.less'
         ],
-        tasks: ['less', 'version']
+        tasks: ['less:dev', 'autoprefixer:dev']
       },
       js: {
         files: [
           '<%= jshint.all %>'
         ],
-        tasks: ['jshint', 'uglify', 'version']
+        tasks: ['jshint', 'concat']
       },
       livereload: {
         // Browser live reloading
         // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
         options: {
-          livereload: false
+          livereload: true
         },
         files: [
-          'assets/css/main.min.css',
-          'assets/js/scripts.min.js',
+          'assets/css/main.css',
+          'assets/js/scripts.js',
           'templates/*.php',
           '*.php'
         ]
       }
-    },
-    clean: {
-      dist: [
-        'assets/css/main.min.css',
-        'assets/js/scripts.min.js'
-      ]
     }
   });
 
-  // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-wp-version');
-
   // Register tasks
   grunt.registerTask('default', [
-    'clean',
-    'less',
-    'uglify',
-    'version'
+    'dev'
   ]);
   grunt.registerTask('dev', [
+    'jshint',
+    'less:dev',
+    //'autoprefixer:dev',
+    'concat',
     'watch'
   ]);
-
+  grunt.registerTask('build', [
+    'jshint',
+    'less:build',
+    //'autoprefixer:build',
+    'uglify',
+    //'modernizr',
+    'version'
+  ]);
 };

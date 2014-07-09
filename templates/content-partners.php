b76e3c7a4e4a0d84f7afee6b45d3799ea7e27cf8
Philip Seyfi <seifip@gmail.com>
2014-07-09 09:51:44 -0400

    partner institutions template

---
+++ templates/content-partners.php
@@ -0,0 +1,21 @@
+<?php while (have_posts()) : the_post(); ?>
+  <h4>We believe in offering the highest quality surgical videos, created in partnership with leading schools and professionals who share our commitment to excellence in medical instruction.</p>
+  <h2>Partners and early adopters</h4>
+  <div class="orgs large">
+  	<div class="row">
+	  	<div class="org col-xs-3"><div class="logo"><img src="https://www.edx.org/sites/default/files/school/image/banner/mit_logo_200x101_0.png"></div><div class="lab">Visionary</div></div>
+	  	<div class="org col-xs-3"><div class="logo"><img src="https://www.edx.org/sites/default/files/school/image/banner/harvard_logo_200x101_0.png"></div><div class="lab">Visionary</div></div>
+	  	<div class="org col-xs-3"><div class="logo"><img src="https://www.edx.org/sites/default/files/school/image/banner/berkeley_logo_200x101_0.png"></div><div class="lab">Partner</div></div>
+	  	<div class="org col-xs-3"><div class="logo"><img src="https://www.edx.org/sites/default/files/school/image/banner/ut_logo_200x101_0.png"></div><div class="lab">Partner</div></div>
+	</div>
+  </div>
+  <h2>Journal subscribers</h2>
+  <div class="orgs small">
+  	<div class="row">
+	  	<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/colgatex_logo200x101.png?itok=BBNL_p1g"></div></div>
+	  	<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/smithsonianx_logo200x101.png?itok=4cLkwS1r"></div></div>
+	  	<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/osakaux_logo200x101.jpg?itok=103Wdum2"></div></div>
+	  	<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/gems-200x101.png?itok=n31KRQP2"></div></div>
+	</div>
+  </div>
+<?php endwhile; ?>

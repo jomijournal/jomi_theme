2ae169b733d352eb9a5ff859333b7478bcef06a3
kevin altschuler <kevinaltschuler@gmail.com>
2014-07-10 11:50:24 -0700

    finshed subs

--- templates/content-partners.php
+++ templates/content-partners.php
@@ -1,36 +1,37 @@
 <?php while (have_posts()) : the_post(); ?>
   <h4>We believe in offering the highest quality surgical videos, created in partnership with leading schools and professionals who share our commitment to excellence in medical instruction.</p>
-  <h2>Partners and early adopters</h4>
+  <h2>Tier 1</h4>
+
   <div class="orgs large">
   	<div class="row">
-	  	<a href="http://www.brighamandwomensfaulkner.org/about-us/patient-visitor-information/Ingersoll-Bowditch-Medical-Library.aspx#.U72il_ldVMI" >
+	  	<a href="http://www.brighamandwomensfaulkner.org/about-us/patient-visitor-information/Ingersoll-Bowditch-Medical-Library.aspx" >
 	  		<div class="org col-xs-3">
 	  			<div class="logo">
-	  				<img src="http://squash.jomi.com/wp-content/themes/jomi/assets/img/clients/standardized/brigham.png">
+	  				<img src="/wp-content/themes/jomi/assets/img/clients/standardized/brigham.png">
 	  			</div>
 	  			<div class="lab">
-	  				Visionary
+	  				Ingersoll Bowditch Medical Library
 	  			</div>
 	  		</div>
 	  	</a>
 
-	  	<a href="https://www.countway.harvard.edu/index.html" >
+	  	<a href="http://portals.clevelandclinic.org/library/Home/tabid/5219/Default.aspx" >
 	  		<div class="org col-xs-3"><div class="logo">
-	  			<img src="http://squash.jomi.com/wp-content/themes/jomi/assets/img/clients/standardized/cleveland.png">
+	  			<img src="/wp-content/themes/jomi/assets/img/clients/standardized/cleveland.png">
 	  				</div>
 	  				<div class="lab">
-	  					Visionary
+	  					Cleveland Clinic Alumni Library
 	  				</div>
 	  			</div>
 	  		</a>
 
-	  	<a href="https://www.countway.harvard.edu/index.html" >
+	  	<a href="http://library.med.cornell.edu/" >
 	  		<div class="org col-xs-3">
 	  			<div class="logo">
-	  				<img src="http://squash.jomi.com/wp-content/themes/jomi/assets/img/clients/standardized/cornell.png">
+	  				<img src="/wp-content/themes/jomi/assets/img/clients/standardized/cornell.png">
 	  					</div>
 	  					<div class="lab">
-	  						Partner
+	  						Weill Medical College - Medical Library
 	  					</div>
 	  					</div>
 	  	</a>
@@ -38,27 +39,82 @@
 	  	<a href="https://www.countway.harvard.edu/index.html" >
 	  		<div class="org col-xs-3">
 	  			<div class="logo">
-	  				<img src="http://squash.jomi.com/wp-content/themes/jomi/assets/img/clients/standardized/harvard.png">
+	  				<img src="/wp-content/themes/jomi/assets/img/clients/standardized/harvard.png">
 	  					</div>
 	  					<div class="lab">
-	  						Partner
+	  						Francis A. Countway Library of Medicine
 	  					</div>
 	  					</div>
 	  	</a>
-
 	</div>
   </div>
 
-  <h2>Journal subscribers</h2>
+  <div class="orgs large">
+  	<div class="row">
+	  	<a href="http://www2.massgeneral.org/library/default.asp" >
+	  		<div class="org col-xs-3">
+	  			<div class="logo">
+	  				<img src="/wp-content/themes/jomi/assets/img/clients/standardized/mass general.png">
+	  			</div>
+	  			<div class="lab">
+	  				Treadwell Library
+	  			</div>
+	  		</div>
+	  	</a>
+
+	  	<a href="https://library.mskcc.org/" >
+	  		<div class="org col-xs-3"><div class="logo">
+	  			<img src="/wp-content/themes/jomi/assets/img/clients/standardized/sloan kettering.png">
+	  				</div>
+	  				<div class="lab">
+	  					MSK Cancer Center Library
+	  				</div>
+	  			</div>
+	  		</a>
+
+	  	<a href="http://www.galter.northwestern.edu/" >
+	  		<div class="org col-xs-3">
+	  			<div class="logo">
+	  				<img src="/wp-content/themes/jomi/assets/img/clients/standardized/northwestern.png">
+	  					</div>
+	  					<div class="lab">
+	  						Galter Health Sciences Library
+	  					</div>
+	  					</div>
+	  	</a>
+
+	  	<a href="http://lane.stanford.edu/index.html" >
+	  		<div class="org col-xs-3">
+	  			<div class="logo">
+	  				<img src="/wp-content/themes/jomi/assets/img/clients/standardized/stanford.png">
+	  					</div>
+	  					<div class="lab">
+	  						Lane Medical Library
+	  					</div>
+	  					</div>
+	  	</a>
+	</div>
+  </div>
+
+
+  <h2>Tier 2</h2>
   <div class="orgs small">
 	<div class="row">
-		<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/colgatex_logo200x101.png?itok=BBNL_p1g"></div></div>
-		<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/smithsonianx_logo200x101.png?itok=4cLkwS1r"></div></div>
-		<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/osakaux_logo200x101.jpg?itok=103Wdum2"></div></div>
-		<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/gems-200x101.png?itok=n31KRQP2"></div></div>
-		<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/mexicox-200x110.png?itok=4tJbF2Gh"></div></div>
-		<div class="org col-xs-2"><div class="logo"><img src="https://www.edx.org/sites/default/files/styles/small/public/school/image/logo/imf_logo200x101_0.png?itok=mx7F-aZB"></div></div>
+		<a href = "http://www2.kent.edu/library/index.cfm"><div class="org col-xs-2"><div class="logosmall"><img class = "logopic" src="/wp-content/themes/jomi/assets/img/clients/standardized/kentstate.png"></div></div></a>
+		<a href = "http://library.nmsu.edu/"><div class="org col-xs-2"><div class="logosmall"><img class = "logopic" src="/wp-content/themes/jomi/assets/img/clients/standardized/newmexico.png"></div></div></a>
+		<a href = "http://ahsl.arizona.edu/"><div class="org col-xs-2"><div class="logosmall"><img class = "logopic" src="/wp-content/themes/jomi/assets/img/clients/standardized/arizona.png"></div></div></a>
 	</div>
   </div>
+
+  <h2>Tier 3</h2>
+  <div class="orgs small">
+	<div class="row">
+		<a href = "http://www.jmmc.com/"><div class="org col-xs-2"><div class="logosmall"><img class = "logopic" src="/wp-content/themes/jomi/assets/img/clients/standardized/johnston.png"></div></div></a>
+		<a href = "http://www.nvrh.org/interior.php/pid/6/sid/51"><div class="org col-xs-2"><div class="logosmall"><img class = "logopic" src="/wp-content/themes/jomi/assets/img/clients/standardized/vermont.png"></div></div></a>
+		<a href = "http://bhson.org/health-sciences-library/"><div class="org col-xs-2"><div class="logosmall"><img class = "logopic" src="/wp-content/themes/jomi/assets/img/clients/standardized/signature.png"></div></div></a>
+		<a href = "http://www.westernconnecticuthealthnetwork.org/"><div class="org col-xs-2"><div class="logosmall"><img class = "logopic" src="/wp-content/themes/jomi/assets/img/clients/standardized/conneticut.png"></div></div></a>
+	</div>
+  </div>
+
   <div style="text-align:center;"><a href="/pricing" class="btn fat white">Subscription &amp; pricing</a></div>
 <?php endwhile; ?>

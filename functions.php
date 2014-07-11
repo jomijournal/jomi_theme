21deaf55cf8597bf3fd27b0ba68b7b8886810a2c
Philip Seyfi <seifip@gmail.com>
2014-07-11 15:26:00 -0400

    id/name permalinks

--- functions.php
+++ functions.php
@@ -97,6 +97,69 @@ register_post_type('article', array(
 )
 ) ); }
 
+function add_article_rewrite_rules() {
+    add_rewrite_rule('^article/([^/]*)/([^/]*)?','index.php?post_type=article&p=$matches[1]','top');
+    flush_rewrite_rules();
+}
+add_action( 'init', 'add_article_rewrite_rules' );
+
+add_action('init', 'article_rewrite');
+function article_rewrite() {
+  global $wp_rewrite;
+  $queryarg = 'post_type=article&p=';
+  $wp_rewrite->add_rewrite_tag('%article_id%', '([^/]+)', $queryarg);
+  $wp_rewrite->add_rewrite_tag('%article_name%', '([^/]+)', $queryarg);
+  $wp_rewrite->add_permastruct('article', '/article/%article_id%/%article_name%/', false);
+}
+
+add_filter('post_type_link', 'article_permalink', 1, 3);
+function article_permalink($post_link, $id = 0, $leavename) {
+  global $wp_rewrite;
+  $post = &get_post($id);
+  if ( is_wp_error( $post ) )
+    return $post;
+  $newlink = $wp_rewrite->get_extra_permastruct('article');
+  $newlink = str_replace("%article_id%", $post->ID, $newlink);
+  $newlink = str_replace("%article_name%", $post->post_name, $newlink);
+  $newlink = home_url(user_trailingslashit($newlink));
+  return $newlink;
+}
+
+/*add_filter('post_type_link', 'article_permalink', 1, 3);
+function article_permalink($post_link, $id = 0, $leavename) {
+  global $wp_rewrite;
+  $post = &get_post($id);
+  if ( is_wp_error( $post ) )
+    return $post;
+  $newlink = $wp_rewrite->get_extra_permastruct('article');
+  $newlink = str_replace("%article_id%", $post->ID, $newlink);
+  $newlink = str_replace("%article_name%", $post->post_name, $newlink);
+  $newlink = home_url(user_trailingslashit($newlink));
+  return $newlink;
+}*/
+
+/*add_action('init', 'article_rewrite');
+function article_rewrite() {
+  global $wp_rewrite;
+  $queryarg = 'post_type=article&p=';
+  $wp_rewrite->add_rewrite_tag('%article_id%', '([^/]+)', $queryarg);
+  $wp_rewrite->add_rewrite_tag('%article_name%', '([^/]+)', $queryarg);
+  $wp_rewrite->add_permastruct('article', '/article/%article_id%/%article_name%/', false);
+}
+
+add_filter('post_type_link', 'article_permalink', 1, 3);
+function article_permalink($post_link, $id = 0, $leavename) {
+  global $wp_rewrite;
+  $post = &get_post($id);
+  if ( is_wp_error( $post ) )
+    return $post;
+  $newlink = $wp_rewrite->get_extra_permastruct('article');
+  $newlink = str_replace("%article_id%", $post->ID, $newlink);
+  $newlink = str_replace("%article_name%", $post->post_name, $newlink);
+  $newlink = home_url(user_trailingslashit($newlink));
+  return $newlink;
+}*/
+
 /*
 =================================
 CUSTOM SIDEBARS

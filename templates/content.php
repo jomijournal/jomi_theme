c112f5ccfe8bd3f92bd8888de0f92bd1a0e3a6b9
Albert Chen <albert@jomi.com>
2014-07-15 16:38:13 -0400

    fancier all articles page

--- templates/content.php
+++ templates/content.php
@@ -1,4 +1,4 @@
-<article <?php post_class(); ?> style="margin-bottom:35px;">
+<article <?php post_class(); ?>>
 	<?php
 		if ( has_post_thumbnail() ) {
 		?>
@@ -11,5 +11,8 @@
 	<div class='article-overlay'>
 		<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 		<?php get_template_part('templates/entry-meta'); ?>
+		<h4>
+		<?php $a = get_coauthors(); $b = $a[0]; print($b->aim); ?>
+		</h4>
 	</div>
 </article>

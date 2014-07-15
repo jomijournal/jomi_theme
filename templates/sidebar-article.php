ed7434a962ce30c5920398b7edd6fa400626c3b5
Albert Chen <albert@jomi.com>
2014-07-15 16:18:42 -0400

    removed fluff from sidebar-article

--- templates/sidebar-article.php
+++ templates/sidebar-article.php
@@ -1,14 +1,1 @@
-<?php
-/*
-Might be good to move this into a separate widget and follow as standard custom sidebar inclusion practice.
- */
-?>
-<!--time class="published" datetime="<?php echo get_the_time('c'); ?>">
-	Published on <?php echo get_the_date(); ?>
-</time-->
-
-<?php 
-// To add - addthis widget.
-?>
-
 <?php dynamic_sidebar('sidebar-article'); ?>

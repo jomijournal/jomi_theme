739a1e2af290e9add9d3277085c270c452eaa1b4
Albert Chen <albert@jomi.com>
2014-07-21 15:10:38 -0400

    mobile accordion menu

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -14,52 +14,61 @@ global $user;
     <main class="col-sm-12" role="main">
       <header class='site-header'>
       	<div class='row'>
-	      	<div class='col-xs-6'>
+	      	<!--div class='col-xs-6'>
 	        	<div class="logo"><a href="/"><img src="/wp-content/themes/jomi/assets/img/logo.png" alt="Journal of Medical Insight"></a></div>
 	       	 	<input placeholder="Search articles" style="margin-top: -10px;" type="text" name="login" size="30" class="border hidden-xs" id="search-field"/>
 	       	 	<span class="glyphicon glyphicon-search search-icon hidden-xs"></span>
-	       	</div>
+	       	</div-->
 	        <!-- Navbar buttons for desktop -->
-	        <!--div class='col-xs-7'-->
-	        	<div class='hidden-xs col-xs-6'>
-			        <nav class="nav-top">
-			          <ul>
-			          	<?php if(!$user): ?>
-			            <li class="dropdown">
-							<a class="dropdown-toggle border" href="#" data-toggle="dropdown" id="login-btn">Sign&nbsp;in</a>
-							<div class="dropdown-menu pull-right" style="padding: 15px;">
-								<div id="login-form">
-									<span class="label label-danger" id="error-login" style="display:none;">User does not exist</span>
-									<input placeholder="Username" id="user_username" style="margin-bottom: 15px;" type="text" name="login" size="30" />
-									<span class="label label-danger" id="error-password" style="display:none;">Invalid password</span>
-									<input placeholder="Password" id="user_password" style="margin-bottom: 15px;" type="password" name="password" size="30" />
-									<input class="btn fat" style="clear: left; width: 100%;" type="submit" name="commit" value="Sign In" />
-								</div>
-							</div>
-						</li>
-						<?php else: ?>
-						<li><a href="/?logout" id="logout-btn">Sign&nbsp;out</a></li>
-						<?php endif; ?>
+        	<div class='col-xs-12'>
 
-			            <li><a href='/subscribers/' class="hidden-xs<?php 	if( is_page( 'subscribers') ) echo " active"; ?>">Subscribers</a></li>
-			            <li><a href="/pricing" class="hidden-xs<?php 		if( is_page( 'pricing') ) echo " active"; ?>">Pricing</a></li>
-			            <li><a href="/contact" class="hidden-xs<?php 		if( is_page( 'contact') ) echo " active"; ?>">Contact</a></li>
-			            <li><a href="/about" class="hidden-xs<?php 			if( is_page( 'about') ) echo " active"; ?>">About</a></li>
-			            
-			          </ul>
-			        </nav>
-			    </div>
-			    <!-- Collapsable navbar for mobile -->
-			    <div class='col-xs-6 visible-xs mobile-menu'>
-			    	<div class='dropdown alignright'>
-					  <a class='dropdown-toggle' data-toggle='dropdown' href='#'> <span class='glyphicon glyphicon-th-list'></span> </a>
-					  <ul class='dropdown-menu dropdown-menu-right' role='menu'>
-					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Login</a></li>
-					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Pricing</a></li>
-					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Contact</a></li>
-					  </ul>
+        		<div class="logo"><a href="/"><img src="/wp-content/themes/jomi/assets/img/logo.png" alt="Journal of Medical Insight"></a></div>
+	       	 	<input placeholder="Search articles" style="margin-top: -10px;" type="text" name="login" size="30" class="border hidden-xs search" id="search-field"/>
+	       	 	<span class="glyphicon glyphicon-search search-icon hidden-xs"></span>
+
+		        <nav class='nav-top hidden-xs'>
+		          <ul>
+		          	<?php if(!$user): ?>
+		            <li class="dropdown">
+						<a class="dropdown-toggle border" href="#" data-toggle="dropdown" id="login-btn">Sign&nbsp;in</a>
+						<div class="dropdown-menu pull-right" style="padding: 15px;">
+							<div id="login-form">
+								<span class="label label-danger" id="error-login" style="display:none;">User does not exist</span>
+								<input placeholder="Username" id="user_username" style="margin-bottom: 15px;" type="text" name="login" size="30" />
+								<span class="label label-danger" id="error-password" style="display:none;">Invalid password</span>
+								<input placeholder="Password" id="user_password" style="margin-bottom: 15px;" type="password" name="password" size="30" />
+								<input class="btn fat" style="clear: left; width: 100%;" type="submit" name="commit" value="Sign In" />
+							</div>
+						</div>
+					</li>
+					<?php else: ?>
+					<li><a href="/?logout" id="logout-btn">Sign&nbsp;out</a></li>
+					<?php endif; ?>
+
+		            <li><a href='/subscribers/' class="hidden-xs<?php 	if( is_page( 'subscribers') ) echo " active"; ?>">Subscribers</a></li>
+		            <li><a href="/pricing" class="hidden-xs<?php 		if( is_page( 'pricing') ) echo " active"; ?>">Pricing</a></li>
+		            <li><a href="/contact" class="hidden-xs<?php 		if( is_page( 'contact') ) echo " active"; ?>">Contact</a></li>
+		            <li><a href="/about" class="hidden-xs<?php 			if( is_page( 'about') ) echo " active"; ?>">About</a></li>
+		            
+		          </ul>
+		        </nav>
+		    <!--/div-->
+		    <!-- Collapsable navbar for mobile -->
+		    <!--div class='col-xs-6 visible-xs mobile-menu panel-group' id='accordion'-->
+		    	<div class='panel-group mobile-menu visible-xs'>
+			    	<div class='panel'>
+					  <div class='panel-heading'><div class='panel-title'><a data-toggle='collapse' data-parent='#accordion' href='#menu'> <span class='glyphicon glyphicon-th-list'></span> </a></div></div>
+					  <div class='panel-collapse collapse' id='menu'>
+						  <ul class='panel-body'>
+						    <a href="#"><li class='top'>Login</li></a>
+						    <a href='/about'><li>About</li></a>
+						    <a href='/contact'><li>Contact</li></a>
+						    <a href='/pricing'><li>Pricing</li></a>
+						    <a href='/subscribers'><li class='bottom'>Subscribers</li></a>
+						  </ul>
+					  </div>
 					</div>
-			    </div>
+				</div>
 		    </div>
 	    </div>
       </header>

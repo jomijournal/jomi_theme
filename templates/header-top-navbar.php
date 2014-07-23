581f6aa112bc64d554024a732f62f3b03c3e359b
Nikita <me@nikitab.com>
2014-07-23 11:08:09 -0400

    modifying menu

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -9,11 +9,72 @@ global $user;
 	<div class='blackbox background'></div>
 
 <?php endif; ?>
+
+<nav class="navbar container" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
 <div class="container" role="document">
   <div class="content row">
     <main class="col-sm-12" role="main">
       <header class='site-header'>
+
       	<div class='row'>
+
+
+
+
 	      	<!--div class='col-xs-6'>
 	        	<div class="logo"><a href="/"><img src="/wp-content/themes/jomi/assets/img/logo.png" alt="Journal of Medical Insight"></a></div>
 	       	 	<input placeholder="Search articles" style="margin-top: -10px;" type="text" name="login" size="30" class="border hidden-xs" id="search-field"/>
@@ -59,9 +120,21 @@ global $user;
 		    <!--/div-->
 		    <!-- Collapsable navbar for mobile -->
 		    <!--div class='col-xs-6 visible-xs mobile-menu panel-group' id='accordion'-->
+
+
+
 		    	<div class='panel-group mobile-menu visible-xs'>
 			    	<div class='panel'>
-					  <div class='panel-heading'><div class='panel-title'><a data-toggle='collapse' data-parent='#accordion' href='#menu'> <span class='glyphicon glyphicon-th-list'></span> </a></div></div>
+					  <div class='panel-heading'><div class='panel-title'><a data-toggle='collapse' data-parent='#accordion' href='#menu'> 
+					  		<!--
+					  		<span class='glyphicon glyphicon-th-list'>Toggle Navigation</span>
+					  		-->
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+
+					  		</a></div></div>
 					  <div class='panel-collapse collapse row' id='menu'>
 						  <ul class='panel-body'>
 						  	<?php if(!$user): ?>

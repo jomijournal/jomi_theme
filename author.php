1789cba2e34a9f75b7b12a09c3e84e4eb46043e3
Albert Chen <albert@jomi.com>
2014-07-24 17:17:21 -0400

    author archive fix

--- author.php
+++ author.php
@@ -1,4 +1,4 @@
-<?php get_header(); ?>
+<?php get_template_part('templates/page', 'header'); ?>
 
 <div id="content" class="narrowcolumn">
 
@@ -8,26 +8,20 @@
     $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
     ?>
 
-    <h2>About: <?php echo $curauth->nickname; ?></h2>
-    <dl>
-        <dt>Website</dt>
-        <dd><a href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a></dd>
-        <dt>Profile</dt>
-        <dd><?php echo $curauth->user_description; ?></dd>
-    </dl>
+    <h4>About: <?php echo $curauth->nickname; ?></h4>
+    <ul>
+        <!--li>Website</li>
+        <li><a href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a></li-->
+        <li>Institution: <?php echo $curauth->user_description; ?></li>
+    </ul>
 
-    <h2>Posts by <?php echo $curauth->nickname; ?>:</h2>
+    <h4>Posts by <?php echo $curauth->nickname; ?>:</h4>
 
     <ul>
 <!-- The Loop -->
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <li>
-            <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>">
-            <?php the_title(); ?></a>,
-            <?php the_time('d M Y'); ?> in <?php the_category('&');?>
-        </li>
-
+        <?php get_template_part('templates/content', get_post_format()); ?>
     <?php endwhile; else: ?>
         <p><?php _e('No posts by this author.'); ?></p>
 
@@ -37,5 +31,3 @@
 
     </ul>
 </div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>

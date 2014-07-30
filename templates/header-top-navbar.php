53038f50e6fa9d45e411b28eabe81aa0f9137927
Albert Chen <albert@jomi.com>
2014-07-30 17:02:01 -0400

    removed pricing link and added blog link

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -77,10 +77,10 @@ global $user;
 				<li><?php wp_loginout($_SERVER['REQUEST_URI']); ?></li>
 			<?php endif; ?>
 			<li><a href='/login/' class=" active visible-xs">Sign in</a></li>
-			<li><a href='/subscribers/' class="<?php 	if( is_page( 'subscribers') ) echo " active"; ?>">Subscribers</a></li>
-	        <li><a href="/pricing/" class="<?php 		if( is_page( 'pricing') ) echo " active"; ?>">Pricing</a></li>
-	        <li><a href="/contact/" class="<?php 		if( is_page( 'contact') ) echo " active"; ?>">Contact</a></li>
-	        <li><a href="/about/" class="<?php 			if( is_page( 'about') ) echo " active"; ?>">About</a></li>
+	        <li><a href="/contact/" class="<?php 		if( is_page( 'contact') ) echo " active"; ?>"    >Contact</a></li>
+	        <li><a href="http://blog.jomi.com" class=""                                                  >Blog</a></li>
+	        <li><a href='/subscribers/' class="<?php 	if( is_page( 'subscribers') ) echo " active"; ?>">Subscribe</a></li>
+	        <li><a href="/about/" class="<?php 			if( is_page( 'about') ) echo " active"; ?>"      >About</a></li>
 
      </ul>
     </div><!-- /.navbar-collapse -->

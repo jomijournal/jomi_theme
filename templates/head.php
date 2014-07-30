72813c68b3b2cc7ad83266700d1e214ddb4f852c
Albert Chen <albert@jomi.com>
2014-07-30 17:02:13 -0400

    viewport fix for iphones maybe?

--- templates/head.php
+++ templates/head.php
@@ -54,7 +54,7 @@ if($user && isset($_GET["logout"]))
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title><?php wp_title('|', true, 'right'); ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=320, initial-scale=1, user-scalable=yes">
 
 	<!-- TYPEKIT -->
 	<script type="text/javascript" src="//use.typekit.net/juj1iti.js"></script>

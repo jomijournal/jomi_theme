c32eb177053c1fe2e70efeee8307aa61cb474cf4
Albert Chen <albert@jomi.com>
2014-07-30 15:51:54 -0400

    fixed login button styling

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -58,7 +58,7 @@ global $user;
 									<p class="login-remember"><label><input name="rememberme" type="checkbox" id="rememberme" value="forever"> Remember Me</label></p>
 
 									<p class="login-submit">
-										<input type="submit" name="submit" id="submit" class="btn btn-default" value="Log In" onblur="this.style.color='white'">
+										<input type="submit" name="submit" id="submit" class="btn btn-default" value="Log In">
 										<input type="hidden" name="redirect_to" value="/">
 									</p>
 									<p>

17544ad6055bb6b05251e23b4780fd2e475c8fb9
kevtastic <kevinaltschuler@gmail.com>
2014-07-31 17:12:10 -0700

    updating

--- templates/content-single.php
+++ templates/content-single.php
@@ -24,6 +24,7 @@
         });
       });
     </script>
+    <div>PREPRINT</div>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>

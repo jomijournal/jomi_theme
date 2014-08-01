d7c4323efbd2c50499207c1237fe114408d5d7d9
Albert Chen <albert@jomi.com>
2014-08-01 15:21:22 -0400

    smarter front footer

--- templates/front-footer.php
+++ templates/front-footer.php
@@ -3,34 +3,59 @@
 	  <h4>Specialties:</h4>
   </div>
   <div class='row'>
-	  <div class='col-md-12'>
-		  <nav class="nav-bottom">
-		    <ul>
-		      <div class='col-md-4'>
-		      	<li><a href="/articles/" class="border">All</a></li>
-		      </div>
-		      <div class='col-md-4'>
-		      	<li><a href="/orthopedics/" class="border">Orthopedics</a></li>
-		      </div>
-		      <div class='col-md-4'>
-		      	<li><a href="/general/" class="border">General Surgery</a></li>
-		      </div>
-		    </ul>
+  	<nav class="nav-bottom">
+  		<div class='row'>
+	  		<ul>
+				<div class='col-md-4'>
+					<li><a href="/articles/" class="border">All</a></li>
+				</div>
+<?php 
+$args = array(
+  'type' => 'article',
+  'exclude' => '1',
+  'orderby' => 'count',
+  'order' => 'DESC',
+  'hide_empty' => 0
+);
+$categories = get_categories($args);
+foreach($categories as $category) { 
+	$cat_ID_text = "cat-item-" . $category->cat_ID; 
+	$current = (is_category($category->cat_ID)) ? true : false;
 
-		    <ul>
-		      <div class='col-md-4'>
-		      	<li class='coming-soon'><a href="/notifications/?area=ENT" class="border">ENT</a></li>
-		      </div>
-		      <div class='col-md-4'>
-		      	<li class='coming-soon'><a href="/notifications/?area=ophthalmology" class="border">Ophthalmology</a></li>
-		      </div>
-		      <div class='col-md-4'>
-		      	<li class='coming-soon'><a href="/notifications/?area=fundamentals" class="border">Fundamentals</a></li>
-		      </div>
-		    </ul>
-		  </nav>
-	  </div>
-  </div>
+	$post_args = array(
+		'post_type' => 'article',
+		'category' => $category->cat_ID,
+		'post_status' => array('publish', 'preprint')
+	);
+	$posts = get_posts($post_args);
+
+	$coming_soon = (count($posts) == 0) ? true : false;
+	$notification_url = site_url() . "/notifications/?area=" . $category->cat_name;
+
+	$cat_count = 2;
+
+	if($cat_count == 1) { ?>
+<div class='row'>
+	<ul>
+		<div class='col-md-4'>
+			<li class="<?php if($coming_soon) echo 'coming-soon'?>"><a href="<?php if($coming_soon) echo $notification_url; else echo '/'.$category->slug.'/';?>" class="border"><?php echo $category->name; ?></a></li>
+		</div>
+<?php $cat_count++;
+	} elseif ($cat_count == 2) { ?>
+		<div class='col-md-4'>
+			<li class="<?php if($coming_soon) echo 'coming-soon'?>"><a href="<?php if($coming_soon) echo $notification_url; else echo '/'.$category->slug.'/';?>" class="border"><?php echo $category->name; ?></a></li>
+		</div>
+<?php $cat_count++;
+	} elseif ($cat_count == 3) { ?>
+		<div class='col-md-4'>
+			<li class="<?php if($coming_soon) echo 'coming-soon'?>"><a href="<?php if($coming_soon) echo $notification_url; else echo '/'.$category->slug.'/';?>" class="border"><?php echo $category->name; ?></a></li>
+		</div>
+	</ul>
+</div>
+<?php $cat_count = 1; 
+	}
+}
+?>
 </footer>
 
 <script>

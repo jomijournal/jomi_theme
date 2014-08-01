c94e5030156ef483ac44fed0e685b8a51db5ae51
Albert Chen <albert@jomi.com>
2014-08-01 14:44:47 -0400

    simplified php with some more variables

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -12,8 +12,14 @@
   <a href="<?php echo site_url();?>/articles/">All Articles</a>
 </li>
 <?php foreach($categories as $category) { ?>
-  <li class="cat-item cat-item-<?php echo $category->cat_ID?> <?php if(is_category($category->cat_ID)) echo 'current-cat'; ?><?php if($category->count == 0) echo 'coming-soon' ?>">
-    <a href="<?php if($category->count == 0) { echo site_url();?>/notifications/?area=<?php echo $category->cat_name; } else { echo get_category_link($category->cat_ID); }?>"><?php echo $category->cat_name; ?></a>
+<?php 
+$cat_ID_text = "cat-item-" . $category->cat_ID; 
+if(is_category($category->cat_ID)) $current = true; else $current = false;
+if($category->count == 0) $coming_soon = true; else $coming_soon = false;
+$notification_url = site_url() . "/notifications/?area=" . $category->cat_name;
+?>
+  <li class="cat-item <?php echo $cat_ID_text?> <?php if($current) echo 'current-cat'; ?> <?php if($coming_soon) echo 'coming-soon' ?>">
+    <a href="<?php if($coming-soon) { echo $notification_url; } else { echo get_category_link($category->cat_ID); }?>"><?php echo $category->cat_name; ?></a>
   </li>
 <?php } ?>
 </ul>

5528651881f50f768d740af22918fa5917ba231a
Albert Chen <albert@jomi.com>
2014-08-04 14:06:49 -0400

    migrated about sidebar from db to template

--- templates/sidebar-about.php
+++ templates/sidebar-about.php
@@ -1,1 +1,22 @@
-<?php dynamic_sidebar('sidebar-about'); ?>
+<?php #dynamic_sidebar('sidebar-about'); ?>
+
+<h3>JoMI @ Coalition Downtown</h3>
+
+<div class='google-map'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11792.92086402218!2d-71.07043038847668!3d42.35893277872392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e37083006c4a53%3A0xdf33712cae5b4427!2s101+Arch+St!5e0!3m2!1sen!2sus!4v1404931023288" width="650" height="500" frameborder="0" style="border:0"></iframe>
+</div>
+
+
+<h3>Contact Us</h3>
+
+<h4>Librarians</h4>
+Talk to us about early-adopter pricing.
+<a href='mailto:subscribe@jomi.com'>subscribe@jomi.com</a>
+
+<h4>Investors</h4>
+We work with both institutional and private investors.
+<a href='mailto:investor.relations@jomi.com'>investor.relations@jomi.com</a>
+
+<h4>Everyone</h4>
+We love feedback.
+<a href='mailto:contact@jomi.com'>contact@jomi.com</a>

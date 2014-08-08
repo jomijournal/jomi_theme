0fd0742eea8cc908d0e9384194af7027cc69d1c6
Albert Chen <albert@jomi.com>
2014-08-08 10:17:19 -0400

    db table creation (semi stolen code)

--- lib/jomi/article_access.php
+++ lib/jomi/article_access.php
@@ -1,3 +1,39 @@
 <?php
 
+function access_table_install() {
+	global $wpdb;
+	global $access_db_version;
+
+	$table_name = $wpdb->prefix . 'article_access';
+	
+	/*
+	 * We'll set the default character set and collation for this table.
+	 * If we don't do this, some characters could end up being converted 
+	 * to just ?'s when saved in our table.
+	 */
+	$charset_collate = '';
+
+	if ( ! empty( $wpdb->charset ) ) {
+	  $charset_collate = "DEFAULT CHARACTER SET {$wpdb->charset}";
+	}
+
+	if ( ! empty( $wpdb->collate ) ) {
+	  $charset_collate .= " COLLATE {$wpdb->collate}";
+	}
+
+	$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		name tinytext NOT NULL,
+		text text NOT NULL,
+		url VARCHAR(55) DEFAULT '' NOT NULL,
+		UNIQUE KEY id (id)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+
+	add_option( 'access_db_version', $access_db_version );
+}
+
 ?>

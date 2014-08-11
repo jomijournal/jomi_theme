00a94439772c150994eaafb0d863bf7ec1c33875
Albert Chen <albert@jomi.com>
2014-08-11 14:22:31 -0400

    added institution table creation functions, but deactivated them for now

--- lib/jomi/institution_import.php
+++ lib/jomi/institution_import.php
@@ -4,6 +4,57 @@
  * INSTITUTION IMPORT
  */
 
+global $wpdb;
+
+global $institution_db_version;
+$institution_db_version = '1.0';
+
+global $institution_table_name;
+$institution_table_name = $wpdb->prefix . 'institutions';
+
+function institution_table_install() {
+  global $wpdb;
+  global $institution_db_version;
+  global $institution_table_name;
+  
+  /*
+   * We'll set the default character set and collation for this table.
+   * If we don't do this, some characters could end up being converted 
+   * to just ?'s when saved in our table.
+   */
+  $charset_collate = '';
+
+  if ( ! empty( $wpdb->charset ) ) {
+    $charset_collate = "DEFAULT CHARACTER SET {$wpdb->charset}";
+  }
+
+  if ( ! empty( $wpdb->collate ) ) {
+    $charset_collate .= " COLLATE {$wpdb->collate}";
+  }
+
+  $sql = "CREATE TABLE $institution_table_name (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+ /*
+ PUT CORRESPONDING MSSQL COLUMNS HERE
+  */
+    UNIQUE KEY id (id)
+  ) $charset_collate;";
+
+  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+  dbDelta( $sql );
+
+  add_option( 'institution_db_version', $institution_db_version );
+}
+function update_institution_table() {
+  global $institution_db_version;
+  if(get_option('institution_db_version') != $institution_db_version) {
+    institution_table_install();
+  }
+}
+// lets not do this yet
+//add_action('init', 'update_institution_table');
+
+
 // custom settings page
 add_action('admin_menu', 'my_plugin_menu');
 function my_plugin_menu(){
@@ -31,5 +82,6 @@ function import_institutions(){
   
 
 }
+
 
 ?>

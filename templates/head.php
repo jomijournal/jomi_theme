a07e5eb506d8c93718e5e66082f55746d6b50aca
kevtastic <kevinaltschuler@gmail.com>
2014-08-18 13:03:21 -0700

    fixing fb image

--- templates/head.php
+++ templates/head.php
@@ -63,11 +63,11 @@ if($user && isset($_GET["logout"]))
 	$coauthors = get_coauthors();
 	foreach( $coauthors as $coauthor ) { ?>
 	<meta name="citation_author" content="<?php echo $coauthor->last_name; ?>, <?php echo $coauthor->first_name; ?>">
-	<?php } 
-	if(is_home){ ?>
+	<?php } ?>
+
 	<meta property="og:image" content= "/wp-content/themes/jomi/assets/img/fbshare.gif"/>
 	<meta property="og:image:secure_url" content= "/wp-content/themes/jomi/assets/img/fbshare.gif" />
-	<?php } ?>
+
 
 
 	<meta name="citation_journal_title" content="Journal of Medical Insight">

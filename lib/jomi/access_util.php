d189497be290d2289d008ad4edccd7e9c301d000
Albert Chen <albert@jomi.com>
2014-08-19 10:33:24 -0400

    safer default checking in process_access_post_data

--- lib/jomi/access_util.php
+++ lib/jomi/access_util.php
@@ -16,11 +16,11 @@ $default = array(
 	'result_time_end' => -1,
 	'result_time_elapsed' => -1,
 	'result_msg' => '<p>DEFAULT</p>',
-	'check_type' => '',
-	'check_value' => '',
+	'check_type' => 'none',
+	'check_value' => 'none',
 	'priority' => 0,
-	'selector_type' => '',
-	'selector_value' => ''
+	'selector_type' => 'none',
+	'selector_value' => 'none'
 );
 
 /**
@@ -29,10 +29,10 @@ $default = array(
  */
 function process_access_post_data() {
 
-	$result_type = (empty($_POST['result_type'])) ? $default['result_type'] : $_POST['result_type'];
-	$result_time_start = (empty($_POST['result_time_start'])) ? $default['result_time_start'] : $_POST['result_time_start'];
-	$result_time_end = (empty($_POST['result_time_end'])) ? $default['result_time_end'] : $_POST['result_time_end'];
-	$result_time_elapsed = (empty($_POST['result_time_elapsed'])) ? $default['result_time_elapsed'] : $_POST['result_time_elapsed'];
+	$result_type =          (!isset($_POST['result_type'])         || empty($_POST['result_type']))         ? $default['result_type']         : $_POST['result_type'];
+	$result_time_start =    (!isset($_POST['result_time_start'])   || empty($_POST['result_time_start']))   ? $default['result_time_start']   : $_POST['result_time_start'];
+	$result_time_end =      (!isset($_POST['result_time_end'])     || empty($_POST['result_time_end']))     ? $default['result_time_end']     : $_POST['result_time_end'];
+	$result_time_elapsed =  (!isset($_POST['result_time_elapsed']) || empty($_POST['result_time_elapsed'])) ? $default['result_time_elapsed'] : $_POST['result_time_elapsed'];
 	if(empty($_POST['result_msg'])) {
 		switch ($result_type) {
 			case 'DENY':
@@ -52,11 +52,11 @@ function process_access_post_data() {
 	} else {
 		$result_msg = $_POST['result_msg'];
 	}
-	$check_type = (empty($_POST['check_type'])) ? $default['check_type'] : $_POST['check_type'];
-	$check_value = (empty($_POST['check_value'])) ? $default['check_value'] : $_POST['check_value'];
-	$priority = (empty($_POST['priority'])) ? $default['priority'] : $_POST['priority'];
-	$selector_type = (empty($_POST['selector_type'])) ? $default['selector_type'] : $_POST['selector_type'];
-	$selector_value = (empty($_POST['selector_value'])) ? $default['selector_value'] : $_POST['selector_value'];
+	$check_type =     (!isset($_POST['check_type'])     || empty($_POST['check_type']))     ? $default['check_type']     : $_POST['check_type'];
+	$check_value =    (!isset($_POST['check_value'])    || empty($_POST['check_value']))    ? $default['check_value']    : $_POST['check_value'];
+	$priority =       (!isset($_POST['priority'])       || empty($_POST['priority']))       ? $default['priority']       : $_POST['priority'];
+	$selector_type =  (!isset($_POST['selector_type'])  || empty($_POST['selector_type']))  ? $default['selector_type']  : $_POST['selector_type'];
+	$selector_value = (!isset($_POST['selector_value']) || empty($_POST['selector_value'])) ? $default['selector_value'] : $_POST['selector_value'];
 
 	$out = array(
 		'result_type' => $result_type,

defc63178315f587dd004b641839e4799ef9de98
kevtastic <kevinaltschuler@gmail.com>
2014-08-21 16:19:51 -0700

    another method

--- base.php
+++ base.php
@@ -8,11 +8,11 @@ Base is the entry point for most pages.
 
 
 
-  <!--[if lt IE 8]>
+  [if lt IE 8]
     <div class="alert alert-warning">
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
     </div>
-  <![endif]-->
+  [endif]
 
 
   <?php

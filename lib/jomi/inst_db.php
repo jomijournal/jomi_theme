9189f39d32e5c5772147eb33acd92e88adc3967d
Albert Chen <albert@jomi.com>
2014-08-22 13:13:31 -0400

    first version of the institution dbs

---
+++ lib/jomi/inst_db.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * INSTITUTION DB MANAGEMENT
+ */
+
+global $wpdb;
+
+// change this when installing a new table version
+// otherwise, access_table_install will not run every time (this is a good thing)
+global $inst_db_version;
+$inst_db_version = '1.03';
+
+global $inst_table_name;
+$inst_table_name = $wpdb->prefix . 'institutions';
+
+global $inst_location_table_name;
+$inst_location_table_name = $wpdb->prefix . 'institution_locations';
+
+global $inst_ip_table_name;
+$inst_ip_table_name = $wpdb->prefix . 'institution_ips';
+
+/**
+ * create the table that houses access rules
+ * @return [type] [description]
+ */
+function inst_table_install() {
+	global $wpdb;
+	global $inst_db_version;
+	global $inst_table_name;
+	global $inst_location_table_name;
+	global $inst_ip_table_name;
+
+	$charset_collate = '';
+	if ( ! empty( $wpdb->charset ) ) {
+	  $charset_collate = "DEFAULT CHARACTER SET {$wpdb->charset}";
+	}
+	if ( ! empty( $wpdb->collate ) ) {
+	  $charset_collate .= " COLLATE {$wpdb->collate}";
+	}
+	$inst_sql = "CREATE TABLE $inst_table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		name VARCHAR(100) NOT NULL,
+		UNIQUE KEY id (id)
+	) $charset_collate;";
+	$inst_location_sql = "CREATE TABLE $inst_location_table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		inst_id int NOT NULL,
+		description VARCHAR(256) NOT NULL,
+		continent VARCHAR(20) NOT NULL,
+		region VARCHAR(50) NOT NULL,
+		city VARCHAR(50) NOT NULL,
+		zip VARCHAR(10) NOT NULL,
+		UNIQUE KEY id (id)
+	) $charset_collate;";
+	$inst_ip_sql = "CREATE TABLE $inst_ip_table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		location_id int NOT NULL,
+		start int(11) UNSIGNED NOT NULL,
+		end int(11) UNSIGNED NOT NULL,
+		UNIQUE KEY id (id)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta($inst_sql);
+	dbDelta($inst_location_sql);
+	dbDelta($inst_ip_sql);
+
+	check_db_errors();
+
+	add_option( 'inst_db_version', $inst_db_version );
+}
+/**
+ * runs the table install if the version numbers dont match
+ * @return [type] [description]
+ */
+function update_inst_table() {
+	global $inst_db_version;
+	if(get_option('inst_db_version') != $inst_db_version) {
+		inst_table_install();
+	}
+}
+add_action('init', 'update_inst_table');
+
+
+?>

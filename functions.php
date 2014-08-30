66794fb16e664aba25a76da27af5716afb6b5d18
Albert Chen <albert@jomi.com>
2014-08-29 21:02:24 -0400

    added video length on thumbnails

--- functions.php
+++ functions.php
@@ -155,6 +155,57 @@ foreach($includes as $file){
 }
 unset($file, $filepath);
 
+/**
+ * Save post metadata when a post is saved.
+ *
+ * @param int $post_id The ID of the post.
+ */
+function save_vid_length_meta( $post_id ) {
+
+    $type = 'article';
+
+    // If this isn't a 'article' post, don't update it.
+    if ( $type != $_POST['post_type'] ) {
+        return;
+    }
+    $wistia_id = get_field('wistia_id', $post_id);
+    // wistia API call
+    $video_meta = file_get_contents("https://api.wistia.com/v1/medias/" . $wistia_id . ".json?api_password=" . '181632d7604b54b358a5db48a85cec7d7665be6f');
+    $video_meta = json_decode($video_meta);
+
+    //print_r($video_meta);
+    // duration in seconds
+    $duration = $video_meta->duration;
+    $duration = floor($duration);
+    $hours = floor($duration / 3600);
+
+    if($hours > 0) {
+      $minutes = floor(($duration - ($hours * 3600)) / 60);
+      $seconds = $duration % 60;
+      if($minutes < 10) {
+        $minutes = '0' . $minutes;
+      }
+      if($seconds < 10) {
+        $seconds = '0' . $seconds;
+      }
+
+      $vid_length = $hours . ':' . $minutes . ':' . $seconds;
+    } else {
+      $minutes = floor($duration / 60);
+      $seconds = $duration % 60;
+      if($seconds < 10) {
+        $seconds = '0' . $seconds;
+      }
+
+      $vid_length = $minutes . ":" . $seconds;
+    }
+    
+    // - Update the post's metadata.
+    update_post_meta( $post_id, 'vid_length', $vid_length, '0:00');
+
+}
+add_action( 'save_post', 'save_vid_length_meta', 9999, 1 );
+
 // Bug testing only. Not to be used on a production site!!
 /*add_action('wp_footer', 'roots_wrap_info');
 
--- templates/content.php
+++ templates/content.php
@@ -1,21 +1,15 @@
-<?php //require_once('wistiaapi.php');
-//$w = new WistiaApi('181632d7604b54b358a5db48a85cec7d7665be6f');
-//$ID = get_field('wistia_id');
-//echo $w->mediaShow($ID); 
-//echo '<pre>';
-//print_r($w);
-//print_r($w->mediaShow($ID));
-//echo $ID;
-//print_r($w->projectList());
+<?php 
+
+//print_r($vid_length);
 //echo '</pre>';
 ?>
 
 <div class="article-thumbnail">
 	<?php
 		if ( in_array(get_post_status(), array('preprint', 'in_production', 'coming_soon') ) ) {
-
 			// turn post status into pretty english
-			$status = get_post_status();
+			$id = get_the_ID();
+			$status = get_post_status($id);
 			switch ($status) {
 				case 'preprint':
 					$status_text = 'Preprint';
@@ -34,19 +28,35 @@
 			$link = get_permalink();
 		}
 		if ( has_post_thumbnail() ) { ?>
-		<a href="<?php echo $link; ?>" title="<?php the_title_attribute(); ?>" >
+
+			<a href="<?php echo $link; ?>" title="<?php the_title_attribute(); ?>" >
 			<?php the_post_thumbnail('large'); ?>
+
 		<?php } ?>
+
 		<?php if(in_array($status, array('in_production', 'coming_soon'))) { ?>
 			<div class='unavailable'>
 				<h3><?php echo $status_text; ?></h3>
 			</div>
+
 		<?php } else if ($status == 'preprint') { ?>
 			<h5><?php echo $status_text ?></h5>
+
 		<?php } ?>
+
+		<?php if(in_array($status, array('preprint', ''))) {?>
+			<div class="duration">
+				<?php
+					$id = get_the_ID();
+					$vid_length = get_post_meta($id, 'vid_length', false);
+					$vid_length = array_pop($vid_length);
+				?>
+				<p><?php echo $vid_length; ?></p>
+			</div>
+		<?php } ?>
+
 			<div class='article-overlay'>
 				<h3 class="entry-title"><a href="<?php echo $link; ?>"><?php the_title(); ?></a></h3>
-				<!--h3 class="video-duration"><?php //$w->mediaShow($ID)->duration; ?></h3-->
 				<p class="byline vcard">
 				<?php
 					if ( function_exists( 'coauthors_posts_links' ) ) {

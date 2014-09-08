c57038fa9dfda05d3867445494e763b1bb4cb9a4
Albert Chen <albert@jomi.com>
2014-09-08 19:58:44 -0400

    better article arranging on search page

--- index.php
+++ index.php
@@ -6,9 +6,27 @@
   </div>
 <?php endif; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_format()); ?>
-<?php endwhile; ?>
+<?php 
+$exclude = array(
+  'internal_review'
+);
+$status_order = array(
+  'publish', 
+  'preprint', 
+  'in_production', 
+  'coming_soon'
+);
+
+foreach($status_order as $status) {
+  while (have_posts()) : the_post(); 
+     if(in_array(get_post_status(), $exclude)) continue;
+     if($status != get_post_status()) continue;
+     get_template_part('templates/content', get_post_format()); 
+  endwhile; 
+  rewind_posts();
+}
+
+?>
 
 <?php if ($wp_query->max_num_pages > 1) : ?>
   <nav class="post-nav">

2cb22da7e0b0c77c334ff700f3e300aae61d7b7d
Albert Chen <albert@jomi.com>
2014-09-08 16:55:41 -0400

    fixed ajax.js 404

--- lib/jomi/access_db.php
+++ lib/jomi/access_db.php
@@ -123,7 +123,7 @@ function update_rule() {
 //insert_rule(array());
 
 // embed the javascript file that makes the AJAX request
-wp_enqueue_script( 'my-ajax-request', plugin_dir_url( __FILE__ ) . 'js/ajax.js', array( 'jquery' ) );
+wp_enqueue_script( 'my-ajax-request', ABSPATH . 'wp-content/themes/jomi/assets/vendor/jquery/src/ajax.js', array( 'jquery' ) );
 // declare the URL to the file that handles the AJAX request (wp-admin/admin-ajax.php)
 wp_localize_script( 'my-ajax-request', 'MyAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 

36cbfc44e3b96bdec378214e0546a608f819249d
Albert Chen <albert@jomi.com>
2014-09-22 18:16:18 -0400

    full page template no longer shows wordpress header

--- templates/template-full-page.php
+++ templates/template-full-page.php
@@ -4,5 +4,5 @@ Template Name: Full Page (No Sidebar)
 */
 ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
+<?php //get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>

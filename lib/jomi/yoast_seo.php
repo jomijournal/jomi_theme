bb9c3648f650198faef1b225f16a196fceacea53
Albert Chen <albert@jomi.com>
2014-09-23 17:05:07 -0400

    fb share compatibility with other pages

--- lib/jomi/yoast_seo.php
+++ lib/jomi/yoast_seo.php
@@ -4,12 +4,21 @@
  */
 
 function wpseo_load_article_image($url) {
+
+	global $wp_query;
+	$query = $wp_query->query;
+	$post_type = $query['post_type'];
+	//var_dump($post_type);
+
 	// if is article post type
-	if(is_single()) {
-		 $thumb = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()));
-		 //var_dump($thumb);
+	if(is_single() && $post_type == 'article') {
+		$thumb = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()));
+		var_dump($thumb);
+		return $thumb[0];
+	} else {
+		return $url;
 	}
-	return $thumb[0];
+	
 }
 add_filter('wpseo_opengraph_image', 'wpseo_load_article_image', 100, 1);
 

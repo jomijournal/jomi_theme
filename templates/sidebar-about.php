cc5a921129e32c8bcb1a6c90df1c48499b1d612d
Albert Chen <albert@jomi.com>
2014-09-30 20:48:57 -0400

    added ref to newsletter in about page

--- templates/sidebar-about.php
+++ templates/sidebar-about.php
@@ -6,6 +6,8 @@
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11792.92086402218!2d-71.07043038847668!3d42.35893277872392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e37083006c4a53%3A0xdf33712cae5b4427!2s101+Arch+St!5e0!3m2!1sen!2sus!4v1404931023288" width="650" height="500" frameborder="0" style="border:0"></iframe>
 </div>
 
+<h3>Stay Updated</h3>
+<a class="btn fat white newsletter-btn" href="http://eepurl.com/GL-7L">Sign Up for the Newsletter</a>
 
 <h3>Contact Us</h3>
 

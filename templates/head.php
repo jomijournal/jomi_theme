54eb812d2db71014dbbe295fda78f924f60dcc1a
Albert Chen <albert@jomi.com>
2014-11-05 17:22:25 -0500

    opensearch link ref

--- templates/head.php
+++ templates/head.php
@@ -29,6 +29,9 @@
 
 	<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 
+	<!-- OpenSearch -->
+	<link title="<?php echo site_url('/'); ?>" href="<?php echo site_url('/opensearch.xml'); ?>" type="application/opensearchdescription+xml" rel="search">
+
 	<!-- SEGMENT.IO -->
 	<script type="text/javascript">
 	  window.analytics=window.analytics||[],window.analytics.methods=["identify","group","track","page","pageview","alias","ready","on","once","off","trackLink","trackForm","trackClick","trackSubmit"],window.analytics.factory=function(t){return function(){var a=Array.prototype.slice.call(arguments);return a.unshift(t),window.analytics.push(a),window.analytics}};for(var i=0;i<window.analytics.methods.length;i++){var key=window.analytics.methods[i];window.analytics[key]=window.analytics.factory(key)}window.analytics.load=function(t){if(!document.getElementById("analytics-js")){var a=document.createElement("script");a.type="text/javascript",a.id="analytics-js",a.async=!0,a.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.io/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n)}},window.analytics.SNIPPET_VERSION="2.0.9",

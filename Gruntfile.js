ef4ee359fe325cc18cc4ac368ef1a1ff85a97f7e
Albert Chen <albert@jomi.com>
2014-11-10 13:21:39 -0500

    livereload enabled

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,181 +1,196 @@
 'use strict';
 module.exports = function(grunt) {
-  // Load all tasks
-  require('load-grunt-tasks')(grunt);
-  // Show elapsed time
-  require('time-grunt')(grunt);
+	// Load all tasks
+	require('load-grunt-tasks')(grunt);
+	// Show elapsed time
+	require('time-grunt')(grunt);
 
-  var jsFileList = [
-    'assets/vendor/bootstrap/js/transition.js',
-    'assets/vendor/bootstrap/js/alert.js',
-    'assets/vendor/bootstrap/js/button.js',
-    'assets/vendor/bootstrap/js/carousel.js',
-    'assets/vendor/bootstrap/js/collapse.js',
-    'assets/vendor/bootstrap/js/dropdown.js',
-    'assets/vendor/bootstrap/js/modal.js',
-    'assets/vendor/bootstrap/js/tooltip.js',
-    'assets/vendor/bootstrap/js/popover.js',
-    'assets/vendor/bootstrap/js/scrollspy.js',
-    'assets/vendor/bootstrap/js/tab.js',
-    'assets/vendor/bootstrap/js/affix.js',
-    'assets/js/plugins/*.js',
-    'assets/js/_*.js',
-    'assets/js/vendor/*.js',
-    //'assets/vendor/jquery/dist/'
-    'assets/vendor/coindonationwidget.com/coin.js'
-  ];
+	var jsFileList = [
+		'assets/vendor/bootstrap/js/transition.js',
+		'assets/vendor/bootstrap/js/alert.js',
+		'assets/vendor/bootstrap/js/button.js',
+		'assets/vendor/bootstrap/js/carousel.js',
+		'assets/vendor/bootstrap/js/collapse.js',
+		'assets/vendor/bootstrap/js/dropdown.js',
+		'assets/vendor/bootstrap/js/modal.js',
+		'assets/vendor/bootstrap/js/tooltip.js',
+		'assets/vendor/bootstrap/js/popover.js',
+		'assets/vendor/bootstrap/js/scrollspy.js',
+		'assets/vendor/bootstrap/js/tab.js',
+		'assets/vendor/bootstrap/js/affix.js',
+		'assets/js/plugins/*.js',
+		'assets/js/_*.js',
+		'assets/js/vendor/*.js',
+		//'assets/vendor/jquery/dist/'
+		'assets/vendor/coindonationwidget.com/coin.js'
+	];
 
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        'assets/js/*.js',
-        '!assets/js/scripts.js',
-        '!assets/**/*.min.*'
-      ]
-    },
-    less: {
-      dev: {
-        files: {
-          'assets/css/main.min.css': [
-            'assets/less/app.less'
-          ]
-        },
-        options: {
-          compress: true,
-          // LESS source map
-          // To enable, set sourceMap to true and update sourceMapRootpath based on your install
-          sourceMap: true,
-          sourceMapFilename: 'assets/css/main.min.css.map',
-          sourceMapRootpath: '/app/themes/roots/'
-        }
-      },
-      build: {
-        files: {
-          'assets/css/main.min.css': [
-            'assets/less/app.less'
-          ]
-        },
-        options: {
-          compress: true
-        }
-      }
-    },
-    concat: {
-      options: {
-        separator: ';',
-      },
-      dist: {
-        src: [jsFileList],
-        dest: 'assets/js/scripts.js',
-      },
-    },
-    uglify: {
-      dist: {
-        files: {
-          'assets/js/scripts.min.js': [jsFileList]
-        }
-      }
-    },
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12']
-      },
-      dev: {
-        options: {
-          map: {
-            prev: 'assets/css/'
-          }
-        },
-        src: 'assets/css/main.min.css'
-      },
-      build: {
-        src: 'assets/css/main.min.css'
-      }
-    },
-    modernizr: {
-      build: {
-        devFile: 'assets/vendor/modernizr/modernizr.js',
-        outputFile: 'assets/js/vendor/modernizr.min.js',
-        files: {
-          'src': [
-            ['assets/js/scripts.min.js'],
-            ['assets/css/main.min.css']
-          ]
-        },
-        uglify: true,
-        parseFiles: true
-      }
-    },
-    version: {
-      default: {
-        options: {
-          algorith: 'md5',
-          format: true,
-          rename: true,
-          length: 6,
-          manifest: 'assets/manifest.json',
-          querystring: {
-            style: 'roots_css',
-            script: 'roots_js'
-          }
-        },
-        files: {
-          'lib/scripts.php': 'assets/{css,js}/{main,scripts}.min.{css,js}'
-        }
-      }
-    },
-    watch: {
-      less: {
-        files: [
-          'assets/less/*.less',
-          'assets/less/**/*.less'
-        ],
-        tasks: ['less:dev'/*, 'autoprefixer:dev'*/]
-      },
-      js: {
-        files: [
-          jsFileList,
-          '<%= jshint.all %>'
-        ],
-        tasks: ['jshint', 'concat']
-      },
-      livereload: {
-        // Browser live reloading
-        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
-        options: {
-          livereload: true
-        },
-        files: [
-          'assets/css/main.min.css',
-          'assets/js/scripts.min.js',
-          'templates/*.php',
-          '*.php'
-        ]
-      }
-    }
-  });
+	grunt.initConfig({
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'assets/js/*.js',
+				'!assets/js/scripts.js',
+				'!assets/**/*.min.*'
+			]
+		},
+		less: {
+			dev: {
+				files: {
+					'assets/css/main.min.css': [
+						'assets/less/app.less'
+					]
+				},
+				options: {
+					//compress: true,
+					// LESS source map
+					// To enable, set sourceMap to true and update sourceMapRootpath based on your install
+					//sourceMap: true,
+					//sourceMapFilename: 'assets/css/main.min.css.map',
+					//sourceMapRootpath: '/app/themes/roots/'
 
-  // Register tasks
-  grunt.registerTask('default', [
-    'dev'
-  ]);
-  grunt.registerTask('dev', [
-    'jshint',
-    'less:dev',
-    'autoprefixer:dev',
-    'concat',
-    'watch'
-  ]);
-  grunt.registerTask('build', [
-    'jshint',
-    'less:build',
-    'autoprefixer:build',
-    'uglify',
-    'modernizr',
-    'version'
-  ]);
+					style: 'expanded',
+					compass: true
+				}
+			},
+			build: {
+				files: {
+					'assets/css/main.min.css': [
+						'assets/less/app.less'
+					]
+				},
+				options: {
+					compress: true
+				}
+			}
+		},
+		concat: {
+			options: {
+				separator: ';',
+			},
+			dist: {
+				src: [jsFileList],
+				dest: 'assets/js/scripts.js',
+			},
+		},
+		uglify: {
+			dist: {
+				files: {
+					'assets/js/scripts.min.js': [jsFileList]
+				}
+			}
+		},
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12']
+			},
+			dev: {
+				options: {
+					map: {
+						prev: 'assets/css/'
+					}
+				},
+				src: 'assets/css/main.min.css'
+			},
+			build: {
+				src: 'assets/css/main.min.css'
+			}
+		},
+		modernizr: {
+			build: {
+				devFile: 'assets/vendor/modernizr/modernizr.js',
+				outputFile: 'assets/js/vendor/modernizr.min.js',
+				files: {
+					'src': [
+						['assets/js/scripts.min.js'],
+						['assets/css/main.min.css']
+					]
+				},
+				uglify: true,
+				parseFiles: true
+			}
+		},
+		version: {
+			default: {
+				options: {
+					algorith: 'md5',
+					format: true,
+					rename: true,
+					length: 6,
+					manifest: 'assets/manifest.json',
+					querystring: {
+						style: 'roots_css',
+						script: 'roots_js'
+					}
+				},
+				files: {
+					'lib/scripts.php': 'assets/{css,js}/{main,scripts}.min.{css,js}'
+				}
+			}
+		},
+		watch: {
+			less: {
+				files: [
+					'assets/less/*.less',
+					'assets/less/**/*.less'
+				],
+				tasks: ['less:dev'/*, 'autoprefixer:dev'*/],
+				options: {
+					livereload: 35729
+				}
+			},
+			js: {
+				files: [
+					jsFileList,
+					'<%= jshint.all %>'
+				],
+				tasks: ['jshint', 'concat'],
+				options: {
+					livereload: 35729
+				}
+			},
+			php: {
+				files: ['**/*.php'],
+				options: {
+					livereload: 35729
+				}
+			},
+			livereload: {
+				// Browser live reloading
+				// https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+				options: {
+					livereload: true
+				},
+				files: [
+					'assets/css/main.min.css',
+					'assets/js/scripts.min.js',
+					'templates/*.php',
+					'*.php'
+				]
+			}
+		}
+	});
+
+	// Register tasks
+	grunt.registerTask('default', [
+		'dev'
+	]);
+	grunt.registerTask('dev', [
+		'jshint',
+		'less:dev',
+		'autoprefixer:dev',
+		'concat',
+		'watch'
+	]);
+	grunt.registerTask('build', [
+		'jshint',
+		'less:build',
+		'autoprefixer:build',
+		'uglify',
+		'modernizr',
+		'version'
+	]);
 };
--- lib/scripts.php
+++ lib/scripts.php
@@ -12,44 +12,49 @@
  */
 function roots_scripts() {
 
-    $contents = file_get_contents(ABSPATH . '/wp-content/themes/jomi/assets/manifest.json');
+	$contents = file_get_contents(ABSPATH . '/wp-content/themes/jomi/assets/manifest.json');
 
-    $contents = str_replace('assets/css/main.min.css', 'csspath', $contents);
-    $contents = str_replace('assets/js/scripts.min.js', 'jspath', $contents);
-    
+	$contents = str_replace('assets/css/main.min.css', 'csspath', $contents);
+	$contents = str_replace('assets/js/scripts.min.js', 'jspath', $contents);
+	
 
-    $css_hash = json_decode($contents)->csspath->hash;
-    $js_hash = json_decode($contents)->jspath->hash;
+	$css_hash = json_decode($contents)->csspath->hash;
+	$js_hash = json_decode($contents)->jspath->hash;
 
-    wp_enqueue_style('roots_main', site_url('/wp-content/themes/jomi/assets/css/main.min.css?v=') . $css_hash, false, false);
-    wp_enqueue_style('coin-css', site_url('/wp-content/themes/jomi/assets/vendor/coindonationwidget.com/coin.css'), false, false);
-    wp_enqueue_style('font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css', false, false);
-    
-    wp_register_script('roots_scripts', site_url('/wp-content/themes/jomi/assets/js/scripts.min.js?v=') . $js_hash, array(), null, false);
+	wp_enqueue_style('roots_main', site_url('/wp-content/themes/jomi/assets/css/main.min.css?v=') . $css_hash, false, false);
+	wp_enqueue_style('coin-css', site_url('/wp-content/themes/jomi/assets/vendor/coindonationwidget.com/coin.css'), false, false);
+	wp_enqueue_style('font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css', false, false);
+	
+	wp_register_script('roots_scripts', site_url('/wp-content/themes/jomi/assets/js/scripts.min.js?v=') . $js_hash, array(), null, false);
 
-  // jQuery is loaded using the same method from HTML5 Boilerplate:
-  // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
-  // It's kept in the header instead of footer to avoid conflicts with plugins.
-  if (!is_admin() && current_theme_supports('jquery-cdn')) {
-    wp_deregister_script('jquery');
-    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', array(), null, false);
-    add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
-  }
+	wp_register_script('livereload', 'http://localhost:35729/livereload.js', null, false, false);
+	
 
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
+	// jQuery is loaded using the same method from HTML5 Boilerplate:
+	// Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
+	// It's kept in the header instead of footer to avoid conflicts with plugins.
+	if (!is_admin() && current_theme_supports('jquery-cdn')) {
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', array(), null, false);
+		add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
+	}
 
-  //wp_register_script('userapp', '//app.userapp.io/js/userapp.client.js', array(), null, false);
-  wp_register_script('addthis', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-538f69071761b8d2', array(), null, false);
-  wp_register_script('typekit', '//use.typekit.net/wjg6rds.js', array(), null, false);
+	if (is_single() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
+	}
 
-  //wp_enqueue_script('userapp');
-  wp_enqueue_script('addthis');
-  wp_enqueue_script('typekit');
+	//wp_register_script('userapp', '//app.userapp.io/js/userapp.client.js', array(), null, false);
+	wp_register_script('addthis', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-538f69071761b8d2', array(), null, false);
+	wp_register_script('typekit', '//use.typekit.net/wjg6rds.js', array(), null, false);
 
-  wp_enqueue_script('jquery');
-  wp_enqueue_script('roots_scripts');
+	//wp_enqueue_script('userapp');
+	wp_enqueue_script('addthis');
+	wp_enqueue_script('typekit');
+
+	wp_enqueue_script('livereload');
+
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('roots_scripts');
 
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
@@ -58,32 +63,32 @@ add_action('wp_enqueue_scripts', 'roots_scripts', 100);
 
 // http://wordpress.stackexchange.com/a/12450
 function roots_jquery_local_fallback($src, $handle = null) {
-  static $add_jquery_fallback = false;
+	static $add_jquery_fallback = false;
 
-  if ($add_jquery_fallback) {
-    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/vendor/jquery/dist/jquery.min.js"><\/script>\')</script>' . "\n";
-    $add_jquery_fallback = false;
-  }
+	if ($add_jquery_fallback) {
+		echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/vendor/jquery/dist/jquery.min.js"><\/script>\')</script>' . "\n";
+		$add_jquery_fallback = false;
+	}
 
-  if ($handle === 'jquery') {
-    $add_jquery_fallback = true;
-  }
+	if ($handle === 'jquery') {
+		$add_jquery_fallback = true;
+	}
 
-  return $src;
+	return $src;
 }
 add_action('wp_head', 'roots_jquery_local_fallback');
 
 function roots_google_analytics() { ?>
 <script>
-  (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-  function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-  e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-  e.src='//www.google-analytics.com/analytics.js';
-  r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-  ga('create','<?php echo GOOGLE_ANALYTICS_ID; ?>');ga('send','pageview');
+	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+	function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+	e.src='//www.google-analytics.com/analytics.js';
+	r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+	ga('create','<?php echo GOOGLE_ANALYTICS_ID; ?>');ga('send','pageview');
 </script>
 
 <?php }
 if (GOOGLE_ANALYTICS_ID && !current_user_can('manage_options')) {
-  add_action('wp_footer', 'roots_google_analytics', 20);
+	add_action('wp_footer', 'roots_google_analytics', 20);
 }

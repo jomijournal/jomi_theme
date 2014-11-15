915bd2db2fa80b880304adfe149e2588082d62f8
Albert Chen <albert@jomi.com>
2014-11-15 13:57:38 -0500

    pricing page update

--- pages/pricing.php
+++ pages/pricing.php
@@ -6,11 +6,27 @@
 
 <?php 
 
-
+$action = $_GET['action'];
+if(empty($action)) {
 
 ?>
 
 <div class="pricing">
+
+	<?php if(!is_user_logged_in()) { ?>
+	<div class="row">
+		<div class="col-xs-12">
+			<div href="#login" class="login-notification">
+				Please log in before subscribing to JoMI
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="pricing-notification" id="pricing-notification"></div>
+		</div>
+	</div>
 	<div class="row">
 		<div class="col-xs-12">
 			<h1>Subscribe to the World's Highest Quality Surgical Video Journal</h1>
@@ -31,12 +47,12 @@
 			</div>
 			<div class="plan-form">
 				<p>
-					<input id="student-monthly" type="radio" name="student-option" value="1000">
+					<input id="student-monthly" type="radio" name="student-option" period="monthly" value="1000">
 					Monthly &nbsp;&nbsp;
 					($10.00/mo.)
 				</p>
 				<p>
-					<input id="student-annually" type="radio" name="student-option" value="9900" checked>
+					<input id="student-annually" type="radio" name="student-option" period="annually" value="9900" checked>
 					Annually &nbsp;&nbsp;
 					($8.25/mo.)
 				</p>
@@ -60,12 +76,12 @@
 			</div>
 			<div class="plan-form">
 				<p>
-					<input id="resident-monthly" type="radio" name="resident-option" value="10000">
+					<input id="resident-monthly" type="radio" name="resident-option" period="monthly" value="10000">
 					Monthly &nbsp;&nbsp;
 					($100.00/mo.)
 				</p>
 				<p>
-					<input id="resident-annually" type="radio" name="resident-option" value="99900" checked>
+					<input id="resident-annually" type="radio" name="resident-option" period="annually" value="99900" checked>
 					Annually &nbsp;&nbsp;
 					($83.25/mo.)
 				</p>
@@ -89,12 +105,12 @@
 			</div>
 			<div class="plan-form">
 				<p>
-					<input id="attending-monthly" type="radio" name="attending-option" value="20000">
+					<input id="attending-monthly" type="radio" name="attending-option" period="monthly" value="20000">
 					Monthly &nbsp;&nbsp;
 					($200.00/mo.)
 				</p>
 				<p>
-					<input id="attending-annually" type="radio" name="attending-option" value="199800" checked>
+					<input id="attending-annually" type="radio" name="attending-option" period="annually" value="199800" checked>
 					Annually &nbsp;&nbsp;
 					($166.50/mo.)
 				</p>
@@ -129,16 +145,22 @@
 
 <script src="https://checkout.stripe.com/checkout.js"></script>
 <script>
+
+var amount;
+var name;
+var desc;
+var currency = 'USD';
+var plan;
+
 $(function() {
 
 	var handler = StripeCheckout.configure({
-		key: 'pk_test_A9z9qqisgv9VCj8Xp4906AL5'
+		key: '<?php echo get_option("stripe_test_public_api_key"); ?>'
 		, image: '/wp-content/themes/jomi/assets/img/enso_transparent.png'
 		, token: function(token) {
 			// Use the token to create the charge with a server-side script.
 			// You can access the token ID with `token.id`
-			
-
+			stripe_charge(token);
 		}
 	});
 
@@ -146,24 +168,37 @@ $(function() {
 
 		e.preventDefault();
 
-		var price = $(this).parent().parent().parent().find('input[type="radio"]:checked').attr('value');
+		var price = $(this).parent().parent().parent().find('input[type="radio"]:checked');
+		var price_amount = price.attr('value');
 		//price *= 12;
 
 		// default values
-		var name = 'JoMI';
-		var desc = 'Default Subscription';
-		var amount = 99;
+		name = 'JoMI';
+		desc = 'Default Subscription';
+		amount = 99;
 
-
+		// amount update
 		if(this.id == 'student-sub') {
 			desc = 'Student Subscription';
-			amount = price;
+			amount = price_amount;
+			plan = 'student-';
 		} else if (this.id == 'resident-sub') {
 			desc = 'Resident Subscription';
-			amount = price;
+			amount = price_amount;
+			plan = 'resident-';
 		} else if (this.id == 'attending-sub') {
 			desc = 'Attending Subscription';
-			amount = price;
+			amount = price_amount;
+			plan = 'attending-';
+		}
+
+		// description update
+		if(price.attr('period') == 'monthly') {
+			desc += ' - Monthly';
+			plan += 'monthly';
+		} else if(price.attr('period') == 'annually') {
+			desc += ' - Annual';
+			plan += 'annual';
 		}
 
 		// open checkout
@@ -199,4 +234,87 @@ $(function() {
 	});
 });
 
+function stripe_charge(token) {
+
+	console.log(token);
+
+	if(token.bank_account != null) {
+		$.post(MyAjax.ajaxurl, {
+			action: 'stripe-charge'
+
+			, amount: amount
+			, name: name
+			, desc: desc
+			, currency: currency
+			, plan: plan
+
+			, id:       (token.id       == null) ? null : token.id
+			, object:   (token.object   == null) ? null : token.object
+			, livemode: (token.livemode == null) ? null : token.livemode
+			, created:  (token.created  == null) ? null : token.created
+			, type:     (token.type     == null) ? null : token.type
+			, used:     (token.used     == null) ? null : token.used
+			, email:    (token.email    == null) ? null : token.email
+
+			, bank_id:                   (token.bank_account['id'] == null)                   ? null : token.bank_account['id']
+			, bank_object:               (token.bank_account['object'] == null)               ? null : token.bank_account['object']
+			, bank_country:              (token.bank_account['country'] == null)              ? null : token.bank_account['country']
+			, bank_currency:             (token.bank_account['currency'] == null)             ? null : token.bank_account['currency']
+			, bank_default_for_currency: (token.bank_account['default_for_currency'] == null) ? null : token.bank_account['default_for_currency']
+			, bank_last4:                (token.bank_account['last4'] == null)                ? null : token.bank_account['last4']
+			, bank_status:               (token.bank_account['status'] == null)               ? null : token.bank_account['status']
+			, bank_name:                 (token.bank_account['bank_name'] == null)            ? null : token.bank_account['bank_name']
+			, bank_fingerprint:          (token.bank_account['fingerprint'] == null)          ? null : token.bank_account['fingerprint']
+
+		}, function(response) {
+			console.log(response);
+		});
+	} else {
+		$.post(MyAjax.ajaxurl, {
+			action: 'stripe-charge'
+
+			, amount: amount
+			, name: name
+			, desc: desc
+			, currency: currency
+			, plan: plan
+
+			, id:       (token.id       == null) ? null : token.id
+			, object:   (token.object   == null) ? null : token.object
+			, livemode: (token.livemode == null) ? null : token.livemode
+			, created:  (token.created  == null) ? null : token.created
+			, type:     (token.type     == null) ? null : token.type
+			, used:     (token.used     == null) ? null : token.used
+			, email:    (token.email    == null) ? null : token.email
+
+			, card_id:              (token.card['id'] == null)              ? null : token.card['id']
+			, card_object:          (token.card['object'] == null)          ? null : token.card['object']
+			, card_brand:           (token.card['brand'] == null)           ? null : token.card['brand']
+			, card_exp_month:       (token.card['exp_month'] == null)       ? null : token.card['exp_month']
+			, card_exp_year:        (token.card['exp_year'] == null)        ? null : token.card['exp_year']
+			, card_fingerprint:     (token.card['fingerprint'] == null)     ? null : token.card['fingerprint']
+			, card_funding:         (token.card['funding'] == null)         ? null : token.card['funding']
+			, card_last4:           (token.card['last4'] == null)           ? null : token.card['last4']
+			, card_address_city:    (token.card['address_city'] == null)    ? null : token.card['address_city']
+			, card_address_country: (token.card['address_country'] == null) ? null : token.card['address_country']
+			, card_address_line1:   (token.card['address_line1'] == null)   ? null : token.card['address_line1']
+			, card_address_line2:   (token.card['address_line2'] == null)   ? null : token.card['address_line2']
+			, card_address_state:   (token.card['address_state'] == null)   ? null : token.card['address_state']
+			, card_address_zip:     (token.card['address_zip'] == null)     ? null : token.card['address_zip']
+			, card_country:         (token.card['country'] == null)         ? null : token.card['country']
+			, card_customer:        (token.card['customer'] == null)        ? null : token.card['customer']
+			, card_dynamic_last4:   (token.card['dynamic_last4'] == null)   ? null : token.card['dynamic_last4']
+			, card_name:            (token.card['name'] == null)            ? null : token.card['name']
+
+		}, function(response) {
+			console.log(response);
+		});
+	}
+}
+
 </script>
+
+<?php } elseif($action == 'orderplaced') { ?>
+order placed
+
+<?php } ?>

d95c17efdaf99dd8bd9909013a78a776d015665f
Albert Chen <albert@jomi.com>
2014-11-19 15:26:23 -0500

    header links to profile page

--- templates/header.php
+++ templates/header.php
@@ -83,7 +83,7 @@ global $user_stripe_subscribed;
 				<li class="dropdown logout-dropdown" <?php if(!is_user_logged_in()){echo "style='display:none;'";} ?>>
 					<a class="dropdown-toggle" data-toggle="dropdown" id="logout-toggle" href="#" data-original="<span class='glyphicon glyphicon-user' aria-hidden='true'></span><span class='login-text'>&nbsp;&nbsp;Account</span>"><span class='glyphicon glyphicon-user' aria-hidden='true'></span><span class='login-text'>&nbsp;&nbsp;Account</span></a>
 					<div class="dropdown-menu" role="menu">
-						<a href="#">Account Settings</a>
+						<a href="<?php echo site_url('/profile/'); ?>">Account Settings</a>
 						<hr/>
 						<a href="#" data-toggle="tooltip" data-placement="top" title="Feature Coming Soon">Send Invite</a>
 						<a href="<?php echo site_url('/contact/'); ?>">Send Feedback</a>

b4a1ea54b16fc7c8c56f8b31ed9e0fe07c7f7c6c
Albert Chen <albert@jomi.com>
2014-11-19 15:26:15 -0500

    under construction profile page

--- pages/profile.php
+++ pages/profile.php
@@ -8,9 +8,13 @@
 // USE GET VARIABLES!!
 ?>
 
-<div class="profile">
-	<div class="under-construction">
-	</div>
+<div class="under-construction">
+</div>
 
+<div class="profile">
+	
 	<h1>UNDER CONSTRUCTION</h1>
+
+	<strong>Please email us at <a href="mailto:contact@jomi.com">contact@jomi.com</a> to request changes to your profile.</strong>
+	<br><br>
 </div>

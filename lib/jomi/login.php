ba445772885fdcdab450578ed558e289dd6cdc67
Albert Chen <blahoink@gmail.com>
2016-05-05 01:54:31 +0000

    never redirect to wp-admin even if youre an admin

--- lib/jomi/login.php
+++ lib/jomi/login.php
@@ -81,7 +81,9 @@ add_action('lostpassword_footer', 'jomi_login_footer');
  */
 function my_login_redirect( $redirect_to, $request, $user ) {
   //is there a user to check?
-  global $user;
+  if(empty($redirect_to)) return home_url();
+  else return $redirect_to;
+  /*global $user;
   if ( isset( $user->roles ) && is_array( $user->roles ) ) {
     //check for admins
     if ( in_array( 'administrator', $user->roles ) ) {
@@ -93,7 +95,7 @@ function my_login_redirect( $redirect_to, $request, $user ) {
     }
   } else {
     return $redirect_to;
-  }
+  }*/
 }
 
 add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
